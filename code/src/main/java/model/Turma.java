0932c4377803c49e8825d3d426838d46637420ee
DaviAguilar <daviaguilar10@hotmail.com>
2024-09-01 21:02:07 -0300

    Changes by Lenovo

---
+++ code/src/main/java/model/Turma.java
@@ -0,0 +1,58 @@
+package model;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Turma {
+    private String codigoTurma;
+    private Boolean status;
+    private Professor professor;
+    private Disciplina disciplina;
+    private LinkedList<Aluno> listaAlunos;
+    public Turma(){}
+
+    public void adicionarAluno(Aluno aluno) {
+        listaAlunos.add(aluno);
+        if (listaAlunos.size()>= 2) status = true;
+    }
+
+    public void removerAluno(Aluno aluno) {
+        listaAlunos.remove(aluno);
+        if (listaAlunos.size()<=2) {
+            status = false;
+        }
+    }
+
+    public int getNumeroAlunos(){
+        return listaAlunos.size();
+    }
+
+    public String getCodigoDisciplina(){
+        return disciplina.getCodigo();
+    }
+
+    public String getCodigoTurma() {
+        return codigoTurma;
+    }
+
+    public void setCodigoTurma(String codigoTurma) {
+        this.codigoTurma = codigoTurma;
+    }
+
+    public boolean isMatriculado(Aluno aluno) {
+        return listaAlunos.contains(aluno);
+    }
+
+    public void printTurma() {
+        System.out.println("#############################################################");
+        System.out.println("| Disciplina: " + disciplina.getNome());
+        System.out.println("| Obrigatoria: " + disciplina.getObrigatoria().toString());
+        System.out.println("| Professor: " + professor.getNome());
+        System.out.println("| Codigo Turma: " + this.codigoTurma);
+        System.out.println("| Numero de Alunos Matriculadas: " + this.listaAlunos.size());
+        System.out.println("#############################################################");
+    }
+
+
+}
